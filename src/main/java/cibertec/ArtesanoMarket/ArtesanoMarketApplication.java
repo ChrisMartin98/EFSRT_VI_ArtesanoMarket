9f44574d93e18103c3cec568a2adbea0e90fb047
ChrisMartin98 <chmgarcia97@gmail.com>
2024-02-20 21:06:28 -0500

    Creacion proyecto EFSRT - 0%

---
+++ src/main/java/cibertec/ArtesanoMarket/ArtesanoMarketApplication.java
@@ -0,0 +1,13 @@
+package cibertec.ArtesanoMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtesanoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtesanoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/ArtesanoMarket/ArtesanoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.ArtesanoMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtesanoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
